19f00c94e59dd9153d8611797bf4dccb2d8cfd88
Alfonso71196 <alfonso.347471@gmail.com>
2025-05-22 14:17:54 -0600

    Update CREACION DE COMBOS.sql
    Agrega creacion de partidas combos especial

--- CREACION DE COMBOS.sql
+++ CREACION DE COMBOS.sql
@@ -1,92 +1,169 @@
+-- ============================
+-- CREACIÓN DE COMBOS
+-- ============================
 
---- //CREACION DE COMBOS 
+-- Declaración de variables
+DECLARE @id_combo           VARCHAR(10);
+DECLARE @nom_combo          VARCHAR(50);
+DECLARE @fecini             VARCHAR(10);
+DECLARE @fecfin             VARCHAR(10);
+DECLARE @horain             TIME;
+DECLARE @horafin            TIME;
+DECLARE @cliente            VARCHAR(50);
+DECLARE @cant_paste         INT;
+DECLARE @cant_complemento   INT;
+DECLARE @descuento1         FLOAT;
+DECLARE @descuento2         FLOAT;
 
-DECLARE @id_combo varchar(10);
-DECLARE @nom_combo varchar(50);
-DECLARE @fecini varchar(10);
-DECLARE @fecfin varchar(10);
-DECLARE @horain time;
-DECLARE @horafin time;
-DECLARE @cliente varchar (50);
-DECLARE @cant_paste varchar (50);
---DECLARE @cant_complemento varchar (50);
-DECLARE @descuento1 float;
---DECLARE @descuento2 float;
+-- Especial
+DECLARE @de1                FLOAT;
+DECLARE @de2                FLOAT;
+DECLARE @de3                FLOAT;
+
+-- Asignación de valores
+SET @id_combo         = '47';
+SET @nom_combo        = 'COMBO REPSOL';
+SET @fecini           = '20250526';
+SET @fecfin           = '20250831';
+SET @horain           = '00:00:00';
+SET @horafin          = '11:59:59';
+SET @cant_paste       = 2;
+SET @descuento1       = 0;
+SET @cant_complemento = 1;
+SET @descuento2       = 0;
+
+-- Descuentos especiales
+SET @de1 = 62.85714286; -- Refresco
+SET @de2 = 70.45454545; -- Café
+SET @de3 = 60.60606061; -- Agua
+
+-- Cliente (modificar si aplica distinto a SYS)
+SET @cliente = '';
 
 
-SET @id_combo='44';
-SET @nom_combo='NOCHES PASTEKO';
-SET @fecini='20250601'
-SET @fecfin='20250630'
-SET @horain='19:00:00'
-SET @horafin='23:59:59'
-SET @cant_paste=2;
-SET @descuento1=18.18181818;
---SET @cant_complemento=1;
---SET @descuento2=45.193548;
+/* 
+-- ============================
+-- VERIFICAR SI EXISTEN REGISTROS
+-- ============================
 
-SET @cliente=''-- modificar si el cliente es diferente a SYS 
+SELECT * FROM combos WHERE idcombo = '47';
+SELECT * FROM combosdet WHERE idcombo = '47';
+SELECT * FROM combosEspecial WHERE idcombo = '47';
+SELECT * FROM combosArticuloExento WHERE idcombo = '47';
 
+-- ============================
+-- BORRAR REGISTROS EXISTENTES
+-- ============================
 
-/*
---// VERIFICA SI EXISTEN REGISTROS 
-select * 
-from combos
-where idcombo=44;
-
-select *
-from combosdet
-where idcombo=44;
-
-select *
-from combosEspecial
-where idcombo=44;
-
-select *
-from combosArticuloExento
-where idcombo=44;
-
---//BORRA REGISTROS EN CASO DE EXISTIR 
-
-delete from combos where idcombo='44'
-delete from combosdet where idcombo='44'
-delete from combosEspecial where idcombo='39'
-delete from combosArticuloExento where idcombo='39'
+DELETE FROM combos WHERE idcombo = '47';
+DELETE FROM combosdet WHERE idcombo = '47';
+DELETE FROM combosEspecial WHERE idcombo = '47';
+DELETE FROM combosArticuloExento WHERE idcombo = '47';
 */
 
---// CREA CARATULA COMBO
--------------INSERTA EN COMBOS
-insert into combos
-    (idcombo,Descrip,precio,FecIni,FecFin,HoraIni,HoraFin,CantidadRegalo,DescuentoRegalo,Usuario,UsuFecha,UsuHora,SubFamilia,CantFijaUnSabor,cliente)
-select idcombo=@id_combo, Descrip=@nom_combo, precio, FecIni=@fecini, FecFin=@fecfin, HoraIni=@horain, HoraFin=@horafin, CantidadRegalo, DescuentoRegalo, Usuario, UsuFecha, UsuHora, SubFamilia, CantFijaUnSabor, cliente=@cliente
-from combos
-where idcombo='53'
 
---// CREA COMBOSDET 
--------------INSERTA EN COMBOSDET
-insert into combosdet
-    (idcombo,familia,cantidad,descuento,subfamilia)
-select top 1
-    idcombo=@id_combo, familia, cantidad=@cant_paste, descuento=@descuento1, subfamilia
-from prods
-where linea='01' and articulo='0101'
+-- ============================
+-- CREAR CABECERA DEL COMBO
+-- ============================
+INSERT INTO combos (
+    idcombo, Descrip, precio, FecIni, FecFin, HoraIni, HoraFin,
+    CantidadRegalo, DescuentoRegalo, Usuario, UsuFecha, UsuHora,
+    SubFamilia, CantFijaUnSabor, cliente
+)
+SELECT 
+    idcombo           = @id_combo,
+    Descrip           = @nom_combo,
+    precio,
+    FecIni            = @fecini,
+    FecFin            = @fecfin,
+    HoraIni           = @horain,
+    HoraFin           = @horafin,
+    CantidadRegalo,
+    DescuentoRegalo,
+    Usuario,
+    UsuFecha,
+    UsuHora,
+    SubFamilia,
+    CantFijaUnSabor,
+    cliente           = @cliente
+FROM combos
+WHERE idcombo = '53';
+
+
+-- ============================
+-- AGREGAR PRODUCTOS AL COMBO
+-- ============================
+
+-- Producto principal (paste)
+INSERT INTO combosdet (
+    idcombo, familia, cantidad, descuento, subfamilia
+)
+SELECT TOP 1
+    idcombo    = @id_combo,
+    familia,
+    cantidad   = @cant_paste,
+    descuento  = @descuento1,
+    subfamilia
+FROM prods
+WHERE linea = '01' AND articulo = '0101';
+
+-- Complemento (ej. bebida)
+INSERT INTO combosdet (
+    idcombo, familia, cantidad, descuento, subfamilia
+)
+SELECT TOP 1
+    idcombo    = @id_combo,
+    familia,
+    cantidad   = @cant_complemento,
+    descuento  = @descuento2,
+    subfamilia
+FROM prods
+WHERE linea = '07' AND articulo = '0702';
+
+
+-- ============================
+-- AGREGAR PRODUCTOS ESPECIALES
+-- ============================
+INSERT INTO combosEspecial (
+    idCombo, articulo, descrip, descuento, cantidad, familia, subFamilia
+)
+SELECT 
+    idcombo  = @id_combo,
+    articulo,
+    descrip,
+    descuento = CASE
+        WHEN articulo IN (
+            SELECT articulo FROM prods 
+            WHERE linea = '04' AND bloqueado = '0' AND impuesto <> 'SYS'
+        ) THEN @de1
+        WHEN articulo = '0702' THEN @de2
+        WHEN articulo IN (
+            SELECT articulo FROM prods 
+            WHERE linea = '10' AND bloqueado = '0'
+        ) THEN @de3
+        ELSE 0
+    END,
+    cantidad = @cant_complemento,
+    familia,
+    subfamilia
+FROM prods
+WHERE familia = 'PAQUETE01' AND bloqueado = 0;
+
 
 /*
-insert into combosdet
-    (idcombo,familia,cantidad,descuento,subfamilia)
-select top 1
-    idcombo=@id_combo, familia, cantidad=@cant_complemento, descuento=@descuento2, subfamilia
-from prods
-where linea='15' and articulo='50583'*/
+-- ============================
+-- AGREGAR ARTÍCULOS EXENTOS (SI APLICA)
+-- ============================
 
---// CREA ARTICULO EXCENTO (SOLO SI APLICA)
-/*
---select * from prods where familia='PAQUETE02' and articulo<>'0702'
+-- SELECT * FROM prods WHERE familia = 'PAQUETE02' AND articulo <> '0702';
 
-insert into combosArticuloExento 
-	(idCombo,articulo,descrip)
-select 
-	idcombo=@id_combo,articulo,descrip
-from prods
-where familia='PAQUETE02' and articulo<>'0702'
+INSERT INTO combosArticuloExento (
+    idCombo, articulo, descrip
+)
+SELECT 
+    idcombo  = '47',
+    articulo,
+    descrip
+FROM prods
+WHERE familia = 'PAQUETE01' AND articulo <> '0702';
 */
